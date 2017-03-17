0d593e1c25379e36e84cd72d01df04e33ea9b76e
Nathanael Weber <nathanael.weber@bbcag.ch>
2017-03-17 22:14:34 +0100

    added bootstrap, added third / half stop

--- views/main.controller.js
+++ views/main.controller.js
@@ -2,18 +2,47 @@
  * Created by natha on 16.03.2017.
  */
 Vue.component('aperture-calc', {
-    template: '<div><p>{{ valueBefore }}</p><p>{{ value }}</p><p>{{ valueAfter }}</p><input type="number" v-model="value"></div>',
+    template: `
+    <div>
+        <div>
+            <span class="label label-default">{{ valueBefore }}</span class="label labe-default">
+            <span class="label label-default">{{ thirdBefore[1] }}</span class="label labe-default">
+            <span class="label label-default">{{ halfBefore }}</span class="label labe-default">
+            <span class="label label-default">{{ thirdBefore[0] }}</span class="label labe-default">
+        </div>
+        <div>
+            <span class="label label-primary">{{ value }}</span class="label labe-default">
+        </div>
+        <div>
+            <span class="label label-default">{{ thirdAfter[0] }}</span class="label labe-default">
+            <span class="label label-default">{{ halfAfter }}</span class="label labe-default">
+            <span class="label label-default">{{ thirdAfter[1] }}</span class="label labe-default">
+            <span class="label label-default">{{ valueAfter }}</span class="label labe-default">
+        </div>
+        <input class="form-control" type="number" v-model="value">
+    </div>
+`,
     data: function (){
         return {
             value: 0,
             valueBefore: 0,
-            valueAfter: 0
+            valueAfter: 0,
+            halfBefore: 0,
+            halfAfter: 0,
+            thirdBefore: [0, 0],
+            thirdAfter: [0, 0]
         }
     },
     watch: {
         value: function (val) {
-            this.valueBefore = Math.sqrt(Math.pow(2, Math.round(log10(Math.pow(val, 2)) / log10(2)) - 1));
-            this.valueAfter = Math.sqrt(Math.pow(2, Math.round(log10(Math.pow(val, 2)) / log10(2)) + 1));
+            val = parseFloat(val);
+            this.valueBefore = aperture_calc(val, - 1);
+            this.valueAfter = aperture_calc(val, 1);
+            this.halfBefore = aperture_calc(val, - 0.5);
+            this.halfAfter = aperture_calc(val, 0.5);
+            this.thirdBefore = [aperture_calc(val, - 0.3), aperture_calc(val, - 0.7)];
+            this.thirdAfter = [aperture_calc(val, 0.3), aperture_calc(val, 0.7)];
+            console.log("\n--------\n");
         }
     }
 });
@@ -24,4 +53,12 @@ var app = new Vue({
 
 function log10(val) {
     return Math.log(val) / Math.LN10;
+}
+
+function get_step(val) {
+    return log10(Math.pow(val, 2)) / log10(2);
+}
+
+function aperture_calc(val, step) {
+    return Math.round(Math.sqrt(Math.pow(2, get_step(val) + step)) * 10) / 10;
 }
