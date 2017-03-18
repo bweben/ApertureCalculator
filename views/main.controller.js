5a06df59a99a245d54b253e9d627a5b58acc8229
Nathanael Weber <nathanael.weber@bbcag.ch>
2017-03-18 12:57:05 +0100

    better Calculations, nicer look

--- views/main.controller.js
+++ views/main.controller.js
@@ -2,25 +2,70 @@
  * Created by natha on 16.03.2017.
  */
 Vue.component('aperture-calc', {
-    template: `
-    <div>
-        <div>
-            <span class="label label-default">{{ valueBefore }}</span class="label labe-default">
-            <span class="label label-default">{{ thirdBefore[1] }}</span class="label labe-default">
-            <span class="label label-default">{{ halfBefore }}</span class="label labe-default">
-            <span class="label label-default">{{ thirdBefore[0] }}</span class="label labe-default">
-        </div>
-        <div>
-            <span class="label label-primary">{{ value }}</span class="label labe-default">
-        </div>
-        <div>
-            <span class="label label-default">{{ thirdAfter[0] }}</span class="label labe-default">
-            <span class="label label-default">{{ halfAfter }}</span class="label labe-default">
-            <span class="label label-default">{{ thirdAfter[1] }}</span class="label labe-default">
-            <span class="label label-default">{{ valueAfter }}</span class="label labe-default">
-        </div>
-        <input class="form-control" type="number" v-model="value">
+    template: `<div class="row">
+    <div class="col-lg-3">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th class="col-md-1">Full stop</th>
+                <th class="col-md-1">Half stop</th>
+                <th class="col-md-1">Third stop</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>{{ valueBefore }}</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{{ thirdBefore[1] }}</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>{{ halfBefore }}</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{{ thirdBefore[0] }}</td>
+            </tr>
+            <tr class="active">
+                <td>{{ value }}</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{{ thirdAfter[0] }}</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>{{ halfAfter }}</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{{ thirdAfter[1] }}</td>
+            </tr>
+            <tr>
+                <td>{{ valueAfter }}</td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
     </div>
+    <div class="form-group col-lg-12">
+        <label for="iAperture">Your Aperture:</label>
+        <input id="iAperture" class="form-control" type="number" v-model="value">
+    </div>
+</div>
 `,
     data: function (){
         return {
@@ -30,19 +75,28 @@ Vue.component('aperture-calc', {
             halfBefore: 0,
             halfAfter: 0,
             thirdBefore: [0, 0],
-            thirdAfter: [0, 0]
+            thirdAfter: [0, 0],
+            newValues: [0, 1/3, 1/2, 2/3, 1]
         }
     },
     watch: {
         value: function (val) {
             val = parseFloat(val);
-            this.valueBefore = aperture_calc(val, - 1);
-            this.valueAfter = aperture_calc(val, 1);
-            this.halfBefore = aperture_calc(val, - 0.5);
-            this.halfAfter = aperture_calc(val, 0.5);
-            this.thirdBefore = [aperture_calc(val, - 1/3), aperture_calc(val, - 2/3)];
-            this.thirdAfter = [aperture_calc(val, 1/3), aperture_calc(val, 2/3)];
-            console.log("\n--------\n");
+            let step = ((get_step(val) * 10) / 10).toString().split(".");
+            if (step.length > 1) {
+                step[1] = "0." + step[1];
+                step[1] = get_closest(parseFloat(step[1]), this.newValues);
+            }
+
+            step = parseInt(step[0]) + (step.length > 1 ? step[1] : 0);
+            step = parseFloat(step);
+
+            this.valueBefore = aperture_calc(val, - 1, step);
+            this.valueAfter = aperture_calc(val, 1, step);
+            this.halfBefore = aperture_calc(val, - 0.5, step);
+            this.halfAfter = aperture_calc(val, 0.5, step);
+            this.thirdBefore = [aperture_calc(val, - 1/3, step), aperture_calc(val, - 2/3, step)];
+            this.thirdAfter = [aperture_calc(val, 1/3, step), aperture_calc(val, 2/3, step)];
         }
     }
 });
@@ -59,6 +113,27 @@ function get_step(val) {
     return log10(Math.pow(val, 2)) / log10(2);
 }
 
-function aperture_calc(val, step) {
+function aperture_calc_without_step(val, step) {
     return Math.round(Math.sqrt(Math.pow(2, get_step(val) + step)) * 10) / 10;
+}
+
+function aperture_calc(val, step, orgStep) {
+    let calcedVal = 0;
+    if (!orgStep) {
+        calcedVal = aperture_calc_without_step(val, step);
+    } else {
+        calcedVal = Math.round(Math.sqrt(Math.pow(2, orgStep + step)) * 10) / 10;
+    }
+    calcedVal = calcedVal >= 10 ? Math.round(calcedVal) : calcedVal;
+    return calcedVal;
+}
+
+function get_closest(num, arr) {
+    let curr = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (Math.abs(num - arr[i]) < Math.abs(num -curr)) {
+            curr = arr[i];
+        }
+    }
+    return curr;
 }
